86d27e1504fd895202571c830e3cfe7e55c3ac27
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-08-27 22:58:56 +0900

    Remove HomeScreen composable

--- app/src/main/java/com/example/pokemonclientapp/ui/home/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pokemonclientapp.ui.home
-
-import androidx.compose.runtime.Composable
-import com.example.pokemonclientapp.ui.screen.search.SearchScreen
-
-@Composable
-fun HomeScreen() {
-    SearchScreen()
-}
