80c70c0ee0830e7a4d7649f9d903dd5374dcfe6b
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-06 22:30:59 +0900

    Fix list UI

--- app/src/main/java/com/example/pokemonclientapp/ui/component/BasicInfoCell.kt
+++ app/src/main/java/com/example/pokemonclientapp/ui/component/BasicInfoCell.kt
@@ -1,18 +1,17 @@
 package com.example.pokemonclientapp.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pokemonclientapp.network.model.PokemonEntriesResponse
-import com.example.pokemonclientapp.ui.theme.Purple80
 
 @Composable
 fun BasicInfoCell(
@@ -24,22 +23,22 @@ fun BasicInfoCell(
         modifier = modifier
             .padding(horizontal = 16.dp, vertical = 10.dp)
             .fillMaxWidth()
-            .background(Purple80)
             .clickable(onClick = {
                 onClickCell(info.entryNumber)
             })
     ) {
         Text(
             modifier = modifier
-                .padding(horizontal = 16.dp, vertical = 10.dp),
-            fontSize = 30.sp,
+                .padding(horizontal = 16.dp),
+            fontSize = 15.sp,
             text = "No. ${info.entryNumber}")
         Spacer(modifier = Modifier.padding(vertical = 2.dp))
         Text(
             modifier = modifier
-                .padding(horizontal = 16.dp, vertical = 10.dp),
-            fontSize = 40.sp,
+                .padding(horizontal = 16.dp),
+            fontSize = 20.sp,
             text = info.pokemonSpecies.name
         )
     }
+    Divider()
 }
