ceff46cd5ed8733eb72e8462ffcdcc691f19cd37
RYUJIKOYAMANO\ryuji <ryujikoyama95@gmail.com>
2023-09-06 22:59:11 +0900

    Fix Search and Detail Screen

--- app/src/main/java/com/example/pokemonclientapp/ui/component/BasicInfoCell.kt
+++ app/src/main/java/com/example/pokemonclientapp/ui/component/BasicInfoCell.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pokemonclientapp.network.model.PokemonEntriesResponse
@@ -28,10 +29,12 @@ fun BasicInfoCell(
             })
     ) {
         Text(
+            fontStyle = FontStyle.Italic,
             fontSize = 15.sp,
             text = "No. ${info.entryNumber}")
         Spacer(modifier = Modifier.padding(vertical = 2.dp))
         Text(
+            fontStyle = FontStyle.Italic,
             fontSize = 20.sp,
             text = info.pokemonSpecies.name
         )
--- app/src/main/java/com/example/pokemonclientapp/ui/detail/DetaiScreen.kt
+++ app/src/main/java/com/example/pokemonclientapp/ui/detail/DetaiScreen.kt
@@ -1,21 +1,25 @@
 package com.example.pokemonclientapp.ui.detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.example.pokemonclientapp.network.model.PokemonInfoResponse
 
 @Composable
 fun DetailScreen(
@@ -31,6 +35,13 @@ fun DetailScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         vm.getPokemonInfo(id.toString())
+        Text(
+            modifier = Modifier
+                .padding(top = 30.dp),
+            text = "${pokemonInfo?.name}",
+            fontSize = 30.sp,
+            fontStyle = FontStyle.Italic
+        )
         AsyncImage(
             modifier = Modifier
                 .width(300.dp)
@@ -47,8 +58,18 @@ fun DetailScreen(
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
-        Button(onClick = onClickButton) {
-            Text(text = "Back")
+        Button(
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+            border = BorderStroke(1.dp, color = Color.Black),
+            onClick = onClickButton
+        ) {
+            Text(
+                fontStyle = FontStyle.Italic,
+                text = "Back"
+            )
         }
     }
 }
